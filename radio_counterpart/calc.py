cba1242a4f2132e10c30c40d1aebe058ad45d994
ru-lilas <kumada.ryota@astr.tohoku.ac.jp>
2024-12-10 22:26:13 +0900

    update:「Synchrotron cooling」を追加

--- radio_counterpart/calc.py
+++ radio_counterpart/calc.py
@@ -31,10 +31,15 @@ def calc_Mdot_Edd(mass):
     value = L_Edd / c**2
     return value
 
-def calc_mag_B(eps_B,ej_mass,v_sh,r_sh,t_decay):
+def calc_mag_B2(eps_B,ej_mass,v_sh,r_sh,t_decay):
     B2 = (eps_B*ej_mass*v_sh / (r_sh**2*t_decay)).to(u.erg/(u.cm)**3)
     print(f'B^2 = {B2:3g}')
-    return np.sqrt(B2)
+    return B2
+
+def calc_rho(M_ej,r_sh,v_sh,tau):
+    value = M_ej / (4*PI*r_sh**2*v_sh*tau)
+    return value
+
 
 M_Edd_sol = calc_Mdot_Edd(M)
 print(f'M_Edd_sol = {M_Edd_sol.to(u.g/u.s):3g} = {(M_Edd_sol/M).value:3g} M_sol {u.year**(-1)}')
@@ -49,34 +54,42 @@ print(f'Ejecta mass = eta * {(M_ej).to(u.g):3g}')
 
 # strength of magnetic field
 r_sh = v_sh * t_rise
-B = calc_mag_B(e_B,M_ej,v_sh,r_sh,tau)
-print(B.value*u.G)
+B2 = calc_mag_B2(e_B,M_ej,v_sh,r_sh,tau)
+B = np.sqrt(B2)
+print(f'B= {B:1g}')
 
-# li_const = [['c',c],['M_sol',M],['m_e',m_e],['m_p',m_p]]
-# df_const = pd.DataFrame(li_const, columns=['Name', 'Value'])
-# print(df_const)
-# energy_density = u.def_unit('energy_density',u.erg / (u.cm)**3)
-# print(f'm_e/m_p = {m_p/m_e :3g}')
-# M_x = 1.0 * M
-# e_Edd = 0.1
-# e_B = 0.01
-# zeta_e = 0.4 # 無衝突加熱における, 熱的陽子の, 熱的電子へのエネルギー供給率
+# mass density of ambient medium
+rho = calc_rho(M_ej,r_sh,v_sh,tau)
+print(f'rho_amb = {rho.to(u.g/(u.cm)**3):3g}')
 
-# L_Edd = 10**38 * M_x  * u.erg / (M*u.s)
-# Md_Edd = (L_Edd*0.1*M_x / (e_Edd*M*c**2)).to(u.g/u.s)
+C = M_ej*v_sh**2 / (2*m_e *c**2)
+print(C)
 
-# print(f'v_sh = {v_sh:3g}')
-# print(f't_rise = {t_rise:3g}')
-# print(f'L_Edd = {L_Edd:3g}')
-# print(f'Mdot_Edd = {Md_Edd:3g}')
+# minimum Lorentz factor
+zeta_e = 0.4
+gamma_factor_min = (zeta_e * m_p * v_sh**2) / (2* m_e * c**2)
+print(f'gamma_factor_min = {gamma_factor_min:3g}')
 
-# M_ej = Md_Edd * t_rec
-# B2 = e_B * Md_Edd * t_rec / (v_sh * t_rise**2 * tau)
-# B2 = M_ej * v_sh / (r_sh**2 * tau)
-# B = np.sqrt(B2.cgs)
-# print(f'{(r_sh/const.au).cgs:3g}\n{M_ej:3g}\n{B2:3g}')
-# print(f'{B:3g}')
-# print(v_sh * t_rise**2 * tau)
-# # print(f'{(e*B).cgs}')
-# print(Md_Edd * t_rec)
-# print(np.sqrt(7636))
+# synchrotron radiation
+def calc_frequency(gamma_factor, B):
+    value = (gamma_factor**2*e*B) / (2*PI*m_e*c)
+    return value
+
+nu_e = calc_frequency(gamma_factor_min,B)
+print(nu_e.to(u.GHz))
+
+# synchrotron cooling
+u_B = B2/(8*PI)
+print(f'u_B = {u_B:3g}')
+def calc_synP(gamma_factor,u_B):
+    # beta2 = 1 - gamma_factor**(-2)
+    # print(beta2)
+    value = (4*sigma_T*c*gamma_factor**2*u_B) / 3.0
+    return value
+cooling_rate = calc_synP(gamma_factor_min,u_B)
+print(f'P = {cooling_rate:2g} = {cooling_rate.to(u.MeV/u.s):2g}')
+
+cooling_time = (gamma_factor_min*m_e*c**2) / cooling_rate
+print(f't_cool = {cooling_time.to(u.min):.3g}')
+
+print(f'Thomson cross-section: {sigma_T:.2g}')
