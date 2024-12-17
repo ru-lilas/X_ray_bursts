7fab7124279dfce5e3619d5ea726e2c4ca4462ef
ru-lilas <kumada.ryota@astr.tohoku.ac.jp>
2024-12-17 16:16:05 +0900

    update: B2を追加

--- radio_counterpart/synchrotron_spectrum/python/calc_B_field.py
+++ radio_counterpart/synchrotron_spectrum/python/calc_B_field.py
@@ -5,5 +5,7 @@ def calc_mag_B2(eps_B,M_ej,v_sh,r_sh,l_sh):
     B2 = ((eps_B*M_ej*v_sh**2) / (r_sh**2*l_sh)).to(u.erg*u.cm**(-3))
     return B2
 
-B = np.sqrt(calc_mag_B2(e_B,M_ej,v_sh,r_sh,l_sh))
+B2 = calc_mag_B2(e_B,M_ej,v_sh,r_sh,l_sh)
+B = np.sqrt(B2)
+
 print(f'B = {B:.2g}')
