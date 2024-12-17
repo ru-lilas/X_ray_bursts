d5711cbda2da10157fc8a7999e29b080ca808b87
ru-lilas <kumada.ryota@astr.tohoku.ac.jp>
2024-12-17 17:20:57 +0900

    update: 磁場の計算コードを分離

--- radio_counterpart/synchrotron_spectrum/python/calc.py
+++ radio_counterpart/synchrotron_spectrum/python/calc.py
@@ -1,37 +1,12 @@
-import os
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-from astropy import units as u
-from astropy import constants as const
-import numpy as np
-from scipy.special import kv
-from scipy.special import gamma
-from scipy.integrate import quad
-from fractions import Fraction
-import numpy as np
-
-FONTSIZE_DEFAULT = 18
-OUTPUT_DIR = './' # output file's path
-
-# physical and mathematical constants
-PI = np.pi
-c = const.c.cgs
-G = const.G.cgs
-M = const.M_sun.cgs
-e = const.e.gauss
-m_e = const.m_e.cgs
-m_p = const.m_p.cgs
-m_u = const.u.cgs
-sigma_T = const.sigma_T.cgs
-
+from imports import *
+fdir = './output'
 v_sh = 0.3*c
 t_rec = 200 * u.min
 t_rise = 3 *u.min
 t_x = 10 * u.s
 tau = 10 * u.min
 e_B = 0.01
+p = 2.5
 def calc_L_Edd(mass):
     value = 4*PI*G*mass*m_p*c / sigma_T
     return value.to(u.erg / u.year)
@@ -40,10 +15,6 @@ def calc_Mdot_Edd(mass):
     L_Edd = calc_L_Edd(mass)
     value = L_Edd / c**2
     return value
-
-def calc_mag_B2(eps_B,M_ej,v_sh,r_sh,l_sh):
-    B2 = ((eps_B*M_ej*v_sh**2) / (r_sh**2*l_sh)).to(u.erg*u.cm**(-3))
-    return B2
 
 def calc_rho(M_ej,r_sh,v_sh,tau):
     value = M_ej / (4*PI*r_sh**2*v_sh*tau)
@@ -74,10 +45,7 @@ M_ej = Mdot_4U1728 * t_rec
 print(f'M_ej / eta = {(M_ej).to(u.g):.2g}')
 print(f'l_sh = {l_sh:.2g}, r_sh = {r_sh:.2g}')
 
-# strength of magnetic field
-B2 = calc_mag_B2(e_B,M_ej,v_sh,r_sh,l_sh)
-B = np.sqrt(B2)
-print(f'B^2 = {B2:.2g}, B= {B:.2g}')
+from calc_B_field import B2, B
 
 # mass density of ambient medium
 rho = calc_rho(M_ej,r_sh,v_sh,tau)
@@ -98,14 +66,17 @@ print(f'gamma_max = {calc_max_gamma(B):.2g}')
 
 ratio_ej_to_rest = M_ej*v_sh**2 / (2*m_e *c**2)
 print(f'E_ej / m_e c^2 = {ratio_ej_to_rest.decompose()}')
+
 def calc_synchrotron_coefficient(p,eps_e,min_gamma):
     val = (p-2)*min_gamma**(p-2)*eps_e*ratio_ej_to_rest
+    # print(f'C = {val:.2g}')
     return val
 
 # minimum Lorentz factor
 zeta_e = 0.4
 gamma_factor_min = (zeta_e * m_p * v_sh**2) / (2* m_e * c**2)
 print(f'gamma_factor_min = {gamma_factor_min:3g}')
+
 
 # synchrotron radiation
 def calc_frequency(gamma_factor, B):
@@ -182,50 +153,50 @@ print(f'Thomson cross-section: {sigma_T:.2g}')
 # df = pd.DataFrame({'nu':nu_range, 'P(nu)':list_P})
 # print(df)
 
-def calc_P_nu_noSSA(B,p,nu,eps_e,min_gamma):
-    C = calc_synchrotron_coefficient(p,eps_e,min_gamma).decompose()
-    eB = (e*B).to((u.erg/u.cm))
-    mc2= (m_e * c**2).to(u.erg)
+# def calc_P_nu_noSSA(B,p,nu,eps_e,min_gamma):
+#     C = calc_synchrotron_coefficient(p,eps_e,min_gamma).decompose()
+#     eB = (e*B).to((u.erg/u.cm))
+#     mc2= (m_e * c**2).to(u.erg)
     
-    f0 = ((np.sqrt(3) * e**2 * eB)/(mc2)).to(u.erg)
-    f1 = C/(p+1)
-    f2 = ((3.0 * eB) / (2.0*PI*m_e*c*nu))**((p-1)*0.5)
-    g1 = gamma((3*p+19)/12)
-    g2 = gamma((3*p-1)/12)
+#     f0 = ((np.sqrt(3) * e**2 * eB)/(mc2)).to(u.erg)
+#     f1 = C/(p+1)
+#     f2 = ((3.0 * eB) / (2.0*PI*m_e*c*nu))**((p-1)*0.5)
+#     g1 = gamma((3*p+19)/12)
+#     g2 = gamma((3*p-1)/12)
 
-    val = f0*f1*f2*g1*g2
-    return val
-    # return val.to(u.erg)
+#     val = f0*f1*f2*g1*g2
+#     return val
+#     # return val.to(u.erg)
 
-def calc_SSA_alpha(p,B,C):
-    eB = (e*B).to(u.erg/u.cm)
-    mc2 = (m_e * c**2).to(u.erg)
-    g1 = gamma((3*p+2)/12)
-    g2 = gamma((3*p+22)/12)
-    f1 = 3**(p+1) * e**6 * c**(p+4) * (eB)**p \
-        / (16 * (2*PI)**(p+2) * (mc2)**(3*p+2))
+# def calc_SSA_alpha(p,B,C):
+#     eB = (e*B).to(u.erg/u.cm)
+#     mc2 = (m_e * c**2).to(u.erg)
+#     g1 = gamma((3*p+2)/12)
+#     g2 = gamma((3*p+22)/12)
+#     f1 = 3**(p+1) * e**6 * c**(p+4) * (eB)**p \
+#         / (16 * (2*PI)**(p+2) * (mc2)**(3*p+2))
 
-p=2.5
-eps_e = 0.4
-min_nu = 100*u.MHz
-max_nu = 100*u.GHz
-list_nu = np.linspace(min_nu,max_nu,10000)
+# p=2.5
+# eps_e = 0.4
+# min_nu = 100*u.MHz
+# max_nu = 100*u.GHz
+# list_nu = np.linspace(min_nu,max_nu,10000)
 # print(convert_P_to_F(calc_P_nu_noSSA(B,p,max_nu,eps_e,gamma_factor_min),d))
 
-list_F_nu=[convert_P_to_F(calc_P_nu_noSSA(B,p,nu,eps_e,gamma_factor_min),d).value \
-            for nu in list_nu]
-# print(list_P_nu)
-df = pd.DataFrame({'nu(GHz)':list_nu, 'F(Jy)':list_F_nu})
-print(df)
-plt.rcParams["font.size"] = FONTSIZE_DEFAULT
-fix, ax = plt.subplots(figsize = (8,8))
-# ax.set_xlim()
-ax.loglog()
-ax.set_xlabel(r'$\nu$(GHz)', size=FONTSIZE_DEFAULT)
-ax.set_ylabel(r'$F_\nu $(Jy)', size=FONTSIZE_DEFAULT)
+# list_F_nu=[convert_P_to_F(calc_P_nu_noSSA(B,p,nu,eps_e,gamma_factor_min),d).value \
+#             for nu in list_nu]
+# # print(list_P_nu)
+# df = pd.DataFrame({'nu(GHz)':list_nu, 'F(Jy)':list_F_nu})
+# print(df)
+# plt.rcParams["font.size"] = FONTSIZE_DEFAULT
+# fix, ax = plt.subplots(figsize = (8,8))
+# # ax.set_xlim()
+# ax.loglog()
+# ax.set_xlabel(r'$\nu$(GHz)', size=FONTSIZE_DEFAULT)
+# ax.set_ylabel(r'$F_\nu $(Jy)', size=FONTSIZE_DEFAULT)
 
-ax.plot(list_nu,list_F_nu, ls='-', lw=4)
-plt.savefig(OUTPUT_DIR+'syn-noSSA-p'+f'{int(p*10)}'+'.png')
+# ax.plot(list_nu,list_F_nu, ls='-', lw=4)
+# plt.savefig(OUTPUT_DIR+'syn-noSSA-p'+f'{int(p*10)}'+'.png')
 
 # print(calc_P_nu_noSSA(10*44,B,2,nu_e))
 # print(((np.sqrt(3)*e**2)/(m_e*c**2)).to(u.fm))
--- radio_counterpart/synchrotron_spectrum/python/imports.py
+++ radio_counterpart/synchrotron_spectrum/python/imports.py
@@ -1,15 +1,19 @@
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from astropy import units as u
 from astropy import constants as const
+import numpy as np
 from scipy.special import kv
+from scipy.special import gamma
 from scipy.integrate import quad
 from fractions import Fraction
 import numpy as np
+
+FONTSIZE_DEFAULT = 18
+OUTPUT_DIR = './' # output file's path
 
 # physical and mathematical constants
 PI = np.pi
@@ -17,27 +21,10 @@ c = const.c.cgs
 G = const.G.cgs
 M = const.M_sun.cgs
 e = const.e.gauss
+hbarc = const.hbar * c
+a = const.alpha
 m_e = const.m_e.cgs
 m_p = const.m_p.cgs
 m_u = const.u.cgs
+mc2 = (m_e*c**2).to(u.erg)
 sigma_T = const.sigma_T.cgs
-
-FONTSIZE_DEFAULT = 18
-OUTPUT_DIR = './' # output file's path
-
-v = float(Fraction(5,3)) # order of kv
-def integrand(xi):
-    return kv(v,xi)
-
-def F(x):
-    val, err = quad(integrand,x,np.inf)
-    return x*val
-
-def calc_nu_c(gamma_factor,B):
-    val = (3.0*gamma_factor**2*e*B) / (4.0*PI*m_e*c)
-    return val
-
-def P(nu,gamma_factor,B):
-    nu_c = calc_nu_c(gamma_factor,B)
-    val = (e**3*B*F(nu/nu_c)) / (m_e*c**2)
-    return val 
