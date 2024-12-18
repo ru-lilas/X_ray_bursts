6c246a3e984fbc005f7e8ae24461112c799335aa
ru-lilas <kumada.ryota@astr.tohoku.ac.jp>
2024-12-18 15:36:20 +0900

    add: パラメータを記載したファイル

---
+++ radio_counterpart/synchrotron_spectrum/python/parameter.py
@@ -0,0 +1,16 @@
+from imports import c,u
+
+# observational value
+v_sh = 0.3*c # shock velocity
+t_rec = 200 * u.min # reccurence time of X-ray burst
+t_rise = 3 *u.min # rising time of radio burst
+t_x = 10 * u.s # duration time of X-ray burst
+tau = 10 * u.min # decay time of radio burst
+d = 4.5*u.kpc # distance from 4U 1728-34
+
+# parameters
+e_B = 0.01
+eps_e = 0.1
+p = 2.5
+eta_acc = 0.1
+zeta_e = 0.4
