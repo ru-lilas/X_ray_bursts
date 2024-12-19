88fc2c2eace1f2556f4844e3a38af7ddcf5e1611
ru-lilas <kumada.ryota@astr.tohoku.ac.jp>
2024-12-19 20:24:17 +0900

    update: optically thickの領域まで描画

--- radio_counterpart/synchrotron_spectrum/python/plot_SSA_intensity.py
+++ radio_counterpart/synchrotron_spectrum/python/plot_SSA_intensity.py
@@ -7,26 +7,42 @@ ax.set_ylabel(r'Intensity (mJy str$^{-1}$)')
 ax.loglog()
 ax.grid(True, which='both')
 
-I_mJy:list = [I.to(u.mJy) for I in df['I']]
-S_mJy:list = [S.to(u.mJy) for S in df['S']]
-df_plt = pd.DataFrame({ 
-    'I':[I.value for I in I_mJy],
-    'S':[S.value for S in S_mJy],
-    'nu':df['nu']
+# I_mJy:list          = [I.to(u.mJy) for I in df_thin['I']]
+# I_mJy_dashed:list   = [I.to(u.mJy) for I in ]
+# S_mJy:list          = [S.to(u.mJy) for S in df_thick['S']]
+
+df_plt_thick = pd.DataFrame({ 
+    'S':[S.value for S in source_func],
+    'nu':nu_full[mask_thick]
+})
+df_plt_thin = pd.DataFrame({ 
+    'I':[I.value for I in intensity],
+    'nu':nu_full[mask_thin]
+})
+df_plt_dashed = pd.DataFrame({ 
+    'I':[I.value for I in intensity_dashed],
+    'nu':nu_full[mask_thick]
 })
 
 ax.plot( \
-    df_plt['nu'],
-    df_plt['I'],
+    df_plt_thin['nu'],
+    df_plt_thin['I'],
     ls='-', lw=3,
     color=RED
 )
 
 ax.plot( \
-    df_plt['nu'],
-    df_plt['S'],
+    df_plt_thick['nu'],
+    df_plt_thick['S'],
     ls='-', lw=3,
     color=BLUE
+)
+
+ax.plot( \
+    df_plt_dashed['nu'],
+    df_plt_dashed['I'],
+    ls='--', lw=3,
+    color=RED
 )
 
 FILENAME = \
