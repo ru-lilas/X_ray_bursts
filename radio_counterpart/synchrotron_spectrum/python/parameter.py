2430b4e1ede74c64bd1af17f19c86a9292521b48
ru-lilas <kumada.ryota@astr.tohoku.ac.jp>
2024-12-19 21:22:10 +0900

    upgrade: eta,eps_e,pはstdinで入力できるようにした

--- radio_counterpart/synchrotron_spectrum/python/parameter.py
+++ radio_counterpart/synchrotron_spectrum/python/parameter.py
@@ -1,4 +1,4 @@
-from imports import c,u
+from imports import *
 
 # observational value
 v_sh = 0.3*c # shock velocity
@@ -9,9 +9,31 @@ tau = 10 * u.min # decay time of radio burst
 d = 4.5*u.kpc # distance from 4U 1728-34
 
 # parameters
-eta = 1.0 # M_ej / (Mdot t_rec)
+eta = float(input(">>eta:")) # M_ej / (Mdot t_rec)
 eps = 0.1 # Mdot / Mdot(Edd)
 eps_B = 0.01 
-eps_e = 0.1
-p = 2.5 # power-law index
+eps_e = float(input(">>eps_e:"))
+p = float(input(">>power-law index:")) # power-law index
 zeta_e = 0.4
+
+# observation
+flux90 = [
+1.18,
+1.16,
+1.18,
+1.10,
+1.48,
+1.18,
+1.18,
+]
+flux55 = [
+1.10,
+1.30,
+1.10,
+0.98,
+1.32,
+1.02,
+1.06,
+]
+band55 = [5.5 for _ in range(len(flux55))]
+band90 = [9.0 for _ in range(len(flux55))]
