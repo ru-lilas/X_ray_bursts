c5eb1ccc0eaf58e64bbf5688d9d9f33dce0ffee8
ru-lilas <kumada.ryota@astr.tohoku.ac.jp>
2024-12-19 15:32:35 +0900

    update: パワースペクトル, 源泉関数の典型値を計算できるように変更

--- radio_counterpart/synchrotron_spectrum/python/calc_SSA_spectrum.py
+++ radio_counterpart/synchrotron_spectrum/python/calc_SSA_spectrum.py
@@ -1,19 +1,17 @@
 from imports import *
 from parameter import *
-from calc import p, r_sh,d, l_sh
+from calc_shock import r_sh,l_sh
 from calc_B_field import B
 from calc_powerlaw import N0
-
-RED = '#FF4B00'
+from calc_Lorentz_factor import gamma_min
 
 # power spectrum for power-law distribution
 def calc_spectrum_pl(p,B,N0,nu):
     eB = (e*B).to(u.erg/u.cm)
-    r_e = (e**2 / mc2).to(u.cm)
     x = (nu/c).to(u.cm**(-1))
     G = gamma((3*p+19)/12)*gamma((3*p-1)/12)
     val = \
-        (np.sqrt(3)*N0*r_e*eB) / (p+1) \
+        (np.sqrt(3)*(N0*r_e*eB).to(u.erg*u.cm**(-3))) / (p+1) \
         *((2*PI*mc2*x)/(3*eB))**(-0.5*(p-1)) \
         *G
     return val
@@ -21,25 +19,22 @@ def calc_spectrum_pl(p,B,N0,nu):
 def calc_noSSA_flux(P,r_sh,l_sh,d):
     # print(f'F/P = {(l_sh*(r_sh/d)**2).to(u.cm)}')
     val = P*l_sh*(r_sh/d)**2
-    return val
+    return val.to(u.mJy)
 
 # absorption coefficient
 def calc_SSA_alpha(p,B,N0,nu):
-    f = (e*B).to(u.erg/u.cm)
-    r_e = e**2 / mc2
-    x = c/nu
+    eB = (e*B).to(u.erg/u.cm)
     g1 = gamma((3*p+2)/12)
     g2 = gamma((3*p+22)/12)
 
     val = \
-        (np.sqrt(3)*mc2**(p-1)*N0*r_e*f*x**2)/(8*PI) \
-        *((3*f*x)/(2*PI*mc2**3))**(0.5*p) \
+        ((np.sqrt(3) * N0 * r_e * eB) / (8*PI*mc2*(nu/c)**2)).to(u.cm**(-1)) \
+        * ((3*eB) / (2*PI*mc2*(nu/c)))**(p/2) \
         *g1*g2
-    return val
+    return val.to(u.cm**(-1))
 
 # Source function
 def calc_SSA_source(p,nu,B):
-    # TODO source functionの計算コードを埋める
     f  = (e*B).to(u.erg/u.cm)
     x = nu/c
 
@@ -51,7 +46,11 @@ def calc_SSA_source(p,nu,B):
     (2*x**2*mc2) / (p+1) \
     *np.sqrt(2*PI*mc2*x/(3*f)) \
     *G
-    return val
+    return val.to(u.erg*u.cm**(-2))
+
+def calc_SSA_flux(S,r_sh,d):
+    return (S*(r_sh/d)**2).to(u.mJy)
+
 
 #  the frequency at (Optical depth) = 1
 def calc_nu0(p,N0,B,l_sh):
@@ -60,32 +59,46 @@ def calc_nu0(p,N0,B,l_sh):
     G = \
         ( gamma((3*p+19)/12)*gamma((3*p-1)/12) )
     val = \
-        ((N0*eB*l_sh*np.sqrt(3)*r_e*(mc2)**(p-1))*G / (8*PI))**(2/(p+4)) \
-        *((3*eB)/(2*PI*mc2**3))**(p/(p+4)) \
+        ((N0*eB*l_sh*np.sqrt(3)*r_e*G) / (8*PI*mc2))**(2/(p+4)) \
+        *((3*eB)/(2*PI*mc2))**(p/(p+4)) \
         *c
     return val.to(u.GHz)
+nu_a = calc_nu0(p,N0,B,l_sh) # SSA frequency
 
-nu0 = calc_nu0(p,N0,B,l_sh)
-print(f'nu0 = {nu0:.2g}')
+# calculate more concrete case
+nu_sample = 100*u.GHz
 
-nu_min = 1.0 * u.GHz
-nu_turn = nu0
-nu_max = 1000 *u.GHz
+P_crit      = calc_spectrum_pl(p,B,N0,nu_sample)
+F_crit      = calc_noSSA_flux(P_crit,r_sh,l_sh,d)
+alpha_crit  = calc_SSA_alpha(p,B,N0,nu_sample)
+S_crit      = calc_SSA_source(p,nu_sample,B)
+F_crit_SSA  = calc_SSA_flux(S_crit,r_sh,d)
+print(
+    f'P_crit = {P_crit:.2g} \n' \
+    f'F_crit = {(F_crit):.2g} = {(F_crit.to(u.erg/u.cm**2)):.2g}\n'
+    f'alpha_nu = {alpha_crit:.2g} \n'
+    f'S_crit = {S_crit:.2g} \n'
+    f'F_crit(SSA) = {F_crit_SSA:.2g}'
+    )
 
+print(f'nu_a = {nu_a:.2g}')
 
-nu_list_opthick = np.linspace(nu_min, nu_turn, 100)
-nu_list_noSSA = np.linspace(nu_min,nu_max,1000)
+# nu_min  = e*B*gamma_min**2 / (2*PI*m_e*c) # frequency at minimum Lorentz factor
+nu_min = 1*u.GHz
+nu_max  = 1000 *u.GHz # arbitrary value
 
-power_noSSA = [calc_spectrum_pl(p,B,N0,nu) for nu in nu_list_noSSA]
-flux_noSSA = [calc_noSSA_flux(P,r_sh,l_sh,d).to(u.mJy) for P in power_noSSA]
+nu_SSA \
+= np.linspace(nu_min, nu_a, 100) # frequency list
+nu_noSSA \
+= np.linspace(nu_a,nu_max,100) # frequency list
 
-source_func = [calc_SSA_source(p,nu,B).to(u.mJy) for nu in nu_list_opthick]
-# S_nu += [ \
-#     calc_noSSA_flux(calc_spectrum_pl(p,B,N0,nu),r_sh,l_sh,d).to(u.mJy)\
-#     for nu in nu_list_noSSA \
-#     ]
-flux_SSA = [(2*PI*S*(r_sh/d)**2).to(u.mJy) for S in source_func]
-# print(flux_noSSA)
-# df = pd.DataFrame({'nu(GHz)':nu_list_opthick, 'F_nu(mJy)':[F.value for F in F_nu]})
-# print(df)
+power_noSSA:list    = [calc_spectrum_pl(p,B,N0,nu) for nu in nu_noSSA]
+flux_noSSA:list     = [calc_noSSA_flux(P,r_sh,l_sh,d) for P in power_noSSA]
 
+source_SSA:list     = [calc_SSA_source(p,nu,B) for nu in nu_SSA]
+flux_SSA:list       = [calc_SSA_flux(S,r_sh,d) for S in source_SSA]
+
+df_noSSA    = pd.DataFrame({'nu':nu_noSSA, 'flux':flux_noSSA})
+df_SSA      = pd.DataFrame({'nu':nu_SSA, 'flux':flux_SSA})
+
+print(df_SSA)
